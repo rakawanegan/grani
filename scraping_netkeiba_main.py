12e3e0bbef8acec5ca989067e00da2857c9cb547
NakagawaRen <ren.nakagawa.r@gmail.com>
2024-02-04 12:19:51 +0900

    feat scraping netkeiba

---
+++ scraping_netkeiba_main.py
@@ -0,0 +1,82 @@
+import os
+import pandas as pd
+import datetime
+import json
+from src.scraping_netkeiba import Results, HorseResults, Peds, Return, update_data, get_race_id_list
+
+
+def update_race_data(START_YEAR, END_YEAR, DATA_RACE_RESULTS, DATA_RETURN_RESULTS) -> list:
+    # レースIDのリストを取得
+    race_ids = get_race_id_list(START_YEAR, END_YEAR)
+    # まずはレース結果のデータを取得
+    Results.info()
+    pre_race_results = pd.read_csv(DATA_RACE_RESULTS) if os.path.exists(DATA_RACE_RESULTS) else pd.DataFrame()
+    race_results = Results.scrape(race_ids)
+    # 払い戻し表のデータを取得
+    Return.info()
+    pre_return_data = pd.read_csv(DATA_RETURN_RESULTS) if os.path.exists(DATA_RETURN_RESULTS) else pd.DataFrame()
+    return_data = Return.scrape(race_ids)
+    # 取得したデータを更新
+    updated_race_results = update_data(pre_race_results, race_results)
+    updated_return_data = update_data(pre_return_data, return_data)
+    # それぞれのデータをCSVファイルに保存
+    updated_race_results.to_csv(DATA_RACE_RESULTS)
+    updated_return_data.to_csv(DATA_RETURN_RESULTS)
+    return race_ids
+
+
+def update_horse_data(DATA_RACE_RESULTS, DATA_HORSE_RESULTS, DATA_PEDS_RESULTS, race_ids) -> list:
+    # 前回取得した馬IDのリストを取得
+    pre_horse_results_index = set(pd.read_csv(DATA_RACE_RESULTS, index_col=0).loc[race_ids, "horse_id"].unique()) if os.path.exists(DATA_RACE_RESULTS) else set()
+    # 馬IDのリストを取得
+    horse_ids = list(set(pd.read_csv(DATA_RACE_RESULTS)["horse_id"].unique()) - pre_horse_results_index)
+    horse_ids = [str(horse_id) for horse_id in horse_ids]
+    # 馬の過去成績のデータを取得
+    HorseResults.info()
+    pre_horse_results = pd.read_csv(DATA_HORSE_RESULTS) if os.path.exists(DATA_HORSE_RESULTS) else pd.DataFrame()
+    horse_results = HorseResults.scrape(horse_ids)
+    # 血統データを取得
+    Peds.info()
+    pre_peds_data = pd.read_csv(DATA_PEDS_RESULTS) if os.path.exists(DATA_PEDS_RESULTS) else pd.DataFrame()
+    peds_data = Peds.scrape(horse_ids)
+    # 取得したデータを更新
+    updated_horse_results = update_data(pre_horse_results, horse_results)
+    updated_peds_data = update_data(pre_peds_data, peds_data)
+    # それぞれのデータをCSVファイルに保存
+    updated_horse_results.to_csv(DATA_HORSE_RESULTS)
+    updated_peds_data.to_csv(DATA_PEDS_RESULTS)
+    return horse_ids
+
+
+def main():
+    START_YEAR = 2019 # スクレイピングする年の開始年
+    END_YEAR = 2020 # スクレイピングする年の終了年
+    DATA_BASE_DIR = 'data/scraped_data'
+    DATA_RACE_RESULTS = f'{DATA_BASE_DIR}/race_results.csv'
+    DATA_RETURN_RESULTS = f'{DATA_BASE_DIR}/return_data.csv'
+    DATA_HORSE_RESULTS = f'{DATA_BASE_DIR}/horse_results.csv'
+    DATA_PEDS_RESULTS = f'{DATA_BASE_DIR}/peds_data.csv'
+    ONLY_HORSE_RESULTS = False # Trueにするとレース結果のデータを取得せず、馬の過去成績のデータだけを取得する(なんらかの理由でレース結果のみデータを持っている場合に使用)
+
+    os.makedirs(DATA_BASE_DIR, exist_ok=True)
+
+    # 前回実行時のメタデータを取得
+    metadata = {}
+    pre_metadata = json.load(open(f'{DATA_BASE_DIR}/metadata.json')) if os.path.exists(f'{DATA_BASE_DIR}/metadata.json') else {}
+    if len(pre_metadata):
+        print('previous execute date info: ', pre_metadata['date'])
+    metadata["pre_metadata"] = pre_metadata
+
+    # レースデータを取得
+    race_ids = update_race_data(START_YEAR, END_YEAR, DATA_RACE_RESULTS, DATA_RETURN_RESULTS) if not ONLY_HORSE_RESULTS else []
+    horse_ids = update_horse_data(DATA_RACE_RESULTS, DATA_HORSE_RESULTS, DATA_PEDS_RESULTS, race_ids)
+
+    # メタデータを更新
+    metadata["date"] =  datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    metadata["append_race_id"] = race_ids
+    metadata["append_horse_id"] = horse_ids
+    with open(f'{DATA_BASE_DIR}/metadata.json', 'w') as f:
+        json.dump(metadata, f, indent=4)
+
+if __name__ == "__main__":
+    main()
---
+++ src/scraping_netkeiba.py
@@ -0,0 +1,288 @@
+import requests
+import time
+import re
+import random
+import pandas as pd
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from urllib.request import urlopen
+
+# this code provides by https://zenn.dev/dijzpeb/books/848d4d8e47001193f3fb/viewer/02_scraping
+class Results:
+    @staticmethod
+    def info():
+        print("This is a class for scraping race results from the netkeiba.com")
+    @staticmethod
+    def scrape(race_id_list):
+        """
+        レース結果データをスクレイピングする関数
+        Parameters:
+        ----------
+        race_id_list : list
+            レースIDのリスト
+        Returns:
+        ----------
+        race_results_df : pandas.DataFrame
+            全レース結果データをまとめてDataFrame型にしたもの
+        """
+        #race_idをkeyにしてDataFrame型を格納
+        race_results = {}
+        for race_id in tqdm(race_id_list):
+            time.sleep(1)
+            try:
+                url = "https://db.netkeiba.com/race/" + race_id
+                # スクレイピング
+                html = requests.get(url)
+                html.encoding = "EUC-JP"
+                # メインとなるテーブルデータを取得
+                df = pd.read_html(html.text)[0]
+                # 列名に半角スペースがあれば除去する
+                df = df.rename(columns=lambda x: x.replace(' ', ''))
+                # 天候、レースの種類、コースの長さ、馬場の状態、日付をスクレイピング
+                soup = BeautifulSoup(html.text, "html.parser")
+                texts = (
+                    soup.find("div", attrs={"class": "data_intro"}).find_all("p")[0].text
+                    + soup.find("div", attrs={"class": "data_intro"}).find_all("p")[1].text
+                )
+                info = re.findall(r'\w+', texts)
+                for text in info:
+                    if text in ["芝", "ダート"]:
+                        df["race_type"] = [text] * len(df)
+                    if "障" in text:
+                        df["race_type"] = ["障害"] * len(df)
+                    if "m" in text:
+                        df["course_len"] = [int(re.findall(r"\d+", text)[-1])] * len(df)
+                    if text in ["良", "稍重", "重", "不良"]:
+                        df["ground_state"] = [text] * len(df)
+                    if text in ["曇", "晴", "雨", "小雨", "小雪", "雪"]:
+                        df["weather"] = [text] * len(df)
+                    if "年" in text:
+                        df["date"] = [text] * len(df)
+                #馬ID、騎手IDをスクレイピング
+                horse_id_list = []
+                horse_a_list = soup.find("table", attrs={"summary": "レース結果"}).find_all(
+                    "a", attrs={"href": re.compile("^/horse")}
+                )
+                for a in horse_a_list:
+                    horse_id = re.findall(r"\d+", a["href"])
+                    horse_id_list.append(horse_id[0])
+                jockey_id_list = []
+                jockey_a_list = soup.find("table", attrs={"summary": "レース結果"}).find_all(
+                    "a", attrs={"href": re.compile("^/jockey")}
+                )
+                for a in jockey_a_list:
+                    jockey_id = re.findall(r"\d+", a["href"])
+                    jockey_id_list.append(jockey_id[0])
+                df["horse_id"] = horse_id_list
+                df["jockey_id"] = jockey_id_list
+                #インデックスをrace_idにする
+                df.index = [race_id] * len(df)
+                race_results[race_id] = df
+            #存在しないrace_idを飛ばす
+            except IndexError:
+                continue
+            except AttributeError: #存在しないrace_idでAttributeErrorになるページもあるので追加
+                continue
+            #wifiの接続が切れた時などでも途中までのデータを返せるようにする
+            except Exception as e:
+                print(e)
+                break
+            #Jupyterで停止ボタンを押した時の対処
+            except:
+                break
+        #pd.DataFrame型にして一つのデータにまとめる
+        race_results_df = pd.concat([race_results[key] for key in race_results])
+        return race_results_df
+    
+
+class HorseResults:
+    @staticmethod
+    def info():
+        print("This is a class for scraping horse's past results from the netkeiba.com")
+    @staticmethod
+    def scrape(horse_id_list):
+        """
+        馬の過去成績データをスクレイピングする関数
+
+        Parameters:
+        ----------
+        horse_id_list : list
+            馬IDのリスト
+
+        Returns:
+        ----------
+        horse_results_df : pandas.DataFrame
+            全馬の過去成績データをまとめてDataFrame型にしたもの
+        """
+
+        #horse_idをkeyにしてDataFrame型を格納
+        horse_results = {}
+        for horse_id in tqdm(horse_id_list):
+            time.sleep(1)
+            try:
+                url = 'https://db.netkeiba.com/horse/' + horse_id
+                df = pd.read_html(url)[3]
+                #受賞歴がある馬の場合、3番目に受賞歴テーブルが来るため、4番目のデータを取得する
+                if df.columns[0]=='受賞歴':
+                    df = pd.read_html(url)[4]
+                df.index = [horse_id] * len(df)
+                horse_results[horse_id] = df
+            except IndexError:
+                continue
+            except Exception as e:
+                print(e)
+                break
+            except:
+                break
+
+        #pd.DataFrame型にして一つのデータにまとめる        
+        horse_results_df = pd.concat([horse_results[key] for key in horse_results])
+
+        return horse_results_df
+    
+
+class Peds:
+    @staticmethod
+    def info():
+        print("This is a class for scraping pedigree data from the netkeiba.com")
+    @staticmethod
+    def scrape(horse_id_list):
+        """
+        血統データをスクレイピングする関数
+
+        Parameters:
+        ----------
+        horse_id_list : list
+            馬IDのリスト
+
+        Returns:
+        ----------
+        peds_df : pandas.DataFrame
+            全血統データをまとめてDataFrame型にしたもの
+        """
+
+        peds_dict = {}
+        for horse_id in tqdm(horse_id_list):
+            time.sleep(1)
+            try:
+                url = "https://db.netkeiba.com/horse/ped/" + horse_id
+                df = pd.read_html(url)[0]
+
+                #重複を削除して1列のSeries型データに直す
+                generations = {}
+                for i in reversed(range(5)):
+                    generations[i] = df[i]
+                    df.drop([i], axis=1, inplace=True)
+                    df = df.drop_duplicates()
+                ped = pd.concat([generations[i] for i in range(5)]).rename(horse_id)
+
+                peds_dict[horse_id] = ped.reset_index(drop=True)
+            except IndexError:
+                continue
+            except Exception as e:
+                print(e)
+                break
+            except:
+                break
+
+        #列名をpeds_0, ..., peds_61にする
+        peds_df = pd.concat([peds_dict[key] for key in peds_dict], axis=1).T.add_prefix('peds_')
+
+        return peds_df
+    
+
+class Return:
+    def info():
+        print("This is a class for scraping return data from the netkeiba.com")
+    @staticmethod
+    def scrape(race_id_list):
+        """
+        払い戻し表データをスクレイピングする関数
+
+        Parameters:
+        ----------
+        race_id_list : list
+            レースIDのリスト
+
+        Returns:
+        ----------
+        return_tables_df : pandas.DataFrame
+            全払い戻し表データをまとめてDataFrame型にしたもの
+        """
+
+        return_tables = {}
+        for race_id in tqdm(race_id_list):
+            time.sleep(1)
+            try:
+                url = "https://db.netkeiba.com/race/" + race_id
+
+                #普通にスクレイピングすると複勝やワイドなどが区切られないで繋がってしまう。
+                #そのため、改行コードを文字列brに変換して後でsplitする
+                f = urlopen(url)
+                html = f.read()
+                html = html.replace(b'<br />', b'br')
+                dfs = pd.read_html(html)
+
+                #dfsの1番目に単勝〜馬連、2番目にワイド〜三連単がある
+                df = pd.concat([dfs[1], dfs[2]])
+
+                df.index = [race_id] * len(df)
+                return_tables[race_id] = df
+            except IndexError:
+                continue
+            except AttributeError: #存在しないrace_idでAttributeErrorになるページもあるので追加
+                continue
+            except Exception as e:
+                print(e)
+                break
+            except:
+                break
+
+        #pd.DataFrame型にして一つのデータにまとめる
+        return_tables_df = pd.concat([return_tables[key] for key in return_tables])
+        return return_tables_df
+    
+
+def update_data(old, new):
+    """
+    Parameters:
+    ----------
+    old : pandas.DataFrame
+        古いデータ
+    new : pandas.DataFrame
+        新しいデータ
+    """
+
+    filtered_old = old[~old.index.isin(new.index)]
+    return pd.concat([filtered_old, new])
+
+
+# this code provides by https://agusblog.net/keiba-ai-scraping/
+def get_race_id_list(year_start:int, year_end:int) -> list:
+    race_id_list = []
+    base_url = "https://db.netkeiba.com/race/"
+    place_list= list(range(1,11)) # 競馬場 01札幌	02函館	03福島	04新潟	05東京	06中山	07中京	08京都	09阪神	10小倉
+    open_loop = 6 # 開催回数分ループ（6回）
+    day_loop = 12 # 開催日数分ループ（12日）
+    race_loop = 12 # レース数分ループ（12R）
+    num_iteration = (year_end - year_start) * len(place_list) * open_loop * day_loop * race_loop
+    with tqdm(range(year_start, year_end), leave=False, total=num_iteration) as pbar:
+        for year in pbar:          
+            for place in place_list:
+                for z in range(open_loop+1):
+                    for y in range(day_loop+1):
+                        for x in range(race_loop):
+                            race_id = str(year)+str(place)+str(z+1).zfill(2)+str(y+1).zfill(2)+str(x+1).zfill(2)
+                            url = base_url + race_id
+                            try:
+                                if requests.get(url).status_code==200:
+                                    race_id_list.append(race_id)
+                            # 失敗したら10秒待機してリトライする リクエストを投げすぎるとエラーになることがある
+                            except requests.exceptions.RequestException as e:
+                                print(f"Error: {e}")
+                                print("Waiting in 10 seconds...")
+                                time.sleep(10 + random.randint(1,3))  # 10秒待機
+                            pbar.set_description(f"Progress race_id: {race_id}")
+                            pbar.update(1)
+                            time.sleep(1)
+    return race_id_list
